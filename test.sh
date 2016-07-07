25281fd25ce2b03305bd39e91702787aea67eec8
John P. McCrae <john@mccr.ae>
2016-07-07 10:43:57 +0100

    Wrong format for wn.rdf

--- test.sh
+++ test.sh
@@ -5,5 +5,5 @@ die() { echo "$@" 1>&2 ; exit 1; }
 xmllint --dtdvalid WN-LMF.dtd example.xml >/dev/null || die "XML not working"
 python wn-json-validate.py example.json || die "JSON not working"
 rapper -i turtle example.ttl >/dev/null || die "RDF not working"
-rapper -i turtle wn.ttl > wn.rdf || die "Ontology not valid"
+rapper -i turtle -o rdfxml-abbrev wn.ttl > wn.rdf || die "Ontology not valid"
 
